8932288d895d71d515137c4e6a2740daae0644cb
Balieiro004 <balieiro004@gmail.com>
2023-02-09 19:45:41 -0300

    Estado e cidade

---
+++ src/main/java/com/balieiro/cursomvc/cursomvc/repositories/CidadeRepository.java
@@ -0,0 +1,9 @@
+package com.balieiro.cursomvc.cursomvc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.balieiro.cursomvc.cursomvc.entities.Cidade;
+
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
